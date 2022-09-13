5b1c35632b87fe36f93a6fb6bce747cf6f8fc602
Idris Akintobi <akintobiidris@gmail.com>
2022-09-13 02:30:13 +0100

    extended log time

--- index.js
+++ index.js
@@ -1,57 +1,53 @@
-const express = require('express')
-const cors = require('cors')
-const watch = require('./watch');
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const watch = require("./watch");
+const app = express();
 
 app.use(cors());
 
-app.use('/app', express.static(__dirname + '/ui/dist'));
+app.use("/app", express.static(__dirname + "/ui/dist"));
 
-let count="1";
+let count = "1";
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    "changed":count
+    changed: count,
   });
-})
+});
 
-app.get('/count', (req, res) => {
+app.get("/count", (req, res) => {
   res.json({
-    "changed":count
+    changed: count,
   });
-})
+});
 
-
-const background=function() {
-    console.log('backgroung executed');
-    setTimeout(background, 5000);
-    count++;
-}
+const background = function () {
+  console.log("background executed");
+  setTimeout(background, 60000);
+  count++;
+};
 
 background();
 
-module.exports = 
-{
-    config:{},
-    server:app,
-    watcher:watch,
-    configFromEnv()
-    {
-        const dotenv = require('dotenv');
-        dotenv.config();
-        this.config.port = process.env.PORT || 3000;
-        this.config.connection = process.env.MONGODB_URL 
-        return this;
-        
-    },
-    start()
-    {
-          this.server.listen(this.config.port, () => {
-            console.log(`MESS (Mongo Event Sourcing) listening at http://localhost:${this.config.port}`);
-          });
+module.exports = {
+  config: {},
+  server: app,
+  watcher: watch,
+  configFromEnv() {
+    const dotenv = require("dotenv");
+    dotenv.config();
+    this.config.port = process.env.PORT || 3000;
+    this.config.connection = process.env.MONGODB_URL;
+    return this;
+  },
+  start() {
+    this.server.listen(this.config.port, () => {
+      console.log(
+        `MESS (Mongo Event Sourcing) listening at http://localhost:${this.config.port}`
+      );
+    });
 
-          this.watcher( this.config.connection).catch(console.dir);
-          return this;
-    }
+    this.watcher(this.config.connection).catch(console.dir);
+    return this;
+  },
 };
-
