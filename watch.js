5de07bd44872d6b64e1ef0759ccbac5e35d31e28
Idris Akintobi <akintobiidris@gmail.com>
2022-09-13 02:29:45 +0100

    refactored

--- watch.js
+++ watch.js
@@ -1,66 +1,42 @@
-const config = require('./action');
+const config = require("./action");
 
 const { MongoClient } = require("mongodb");
 
+module.exports = async function watch(uri) {
+  let client = new MongoClient(uri);
+  client = await client.connect();
 
+  // List all the available database
+  let defaultDb = await client.db();
+  let adminDb = defaultDb.admin();
+  let databases = await adminDb.listDatabases();
 
-module.exports =  async function watch(uri) {
-   
-      let client = new MongoClient(uri);
-      client =  await client.connect();
-      
-        // List all the available databaes
-      let defaultDb= await client.db();
-      let adminDb=defaultDb.admin();
-      let databases= await adminDb.listDatabases();
+  console.log(`Available databases: ${databases.databases}`);
+  for (const element of databases.databases) {
+    //only selected databases
+    let dbConfig = config.databases[element.name];
 
-      console.log("Databases found", databases.databases);
-      for (const element of databases.databases) 
-      {
-          //only selected databases
-        
-          console.log("Congifuring",element.name);
-          let dbConfig=config.databases[element.name];
-          
-          if(!dbConfig)
-          {
-            console.log("database not selected, skip it");
-            continue;
-          }
-          console.debug(dbConfig);
+    if (!dbConfig) {
+      console.log(`${element.name} database not selected, skipping...`);
+      continue;
+    }
+    console.log(`Configuring database ${element.name}`);
 
-          let db=client.db(element.name);
-          let collections= await db.listCollections().toArray();
-          
-          for (const collection of collections) 
-          {
-            
-           
-            if(dbConfig.collections && !dbConfig.collections.hasOwnProperty(collection.name))
-            {
-              console.log("collection not enabled, skip it");
-              continue;
-            }
-            let collectionConf=dbConfig.collections[collection.name];
-            console.debug(collectionConf);
+    let db = client.db(element.name);
+    let collections = await db.listCollections().toArray();
 
-            console.log("..Collection",collection.name);
+    for (const collection of collections) {
+      if (
+        dbConfig.collections &&
+        !dbConfig.collections.hasOwnProperty(collection.name)
+      ) {
+        console.log(`${collection.name} not enabled, skipping...`);
+        continue;
+      }
 
-            let changeStream = db.collection(collection.name).watch();
-            changeStream.on("change", next => config.invoke(element.name,collection.name,"changeHook",[next,config]));
-          }
-
-      };
-    //   const database = client.db("sample_mflix");
-    //   changeStream = database.watch();
-
-    //   changeStream.on("change", next => {
-    //     // process any change event
-    //     console.log("received a change to the collection: \t", next);
-    //   });
-    //   // use a timeout to ensure the listener is registered before the insertOne
-    //   // operation is called.
-     
-      
-  
-  };
+      let changeStream = db.collection(collection.name).watch();
+      console.log(`Watching ${collection.name} for changes...`);
+      changeStream.on("change", (next) => config.invoke(next));
+    }
+  }
+};
